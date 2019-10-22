0d42bbba352f5e3d36a5a84cb99491b7c71dc147
Vannida Lim <vannida.lim@gmail.com>
2019-10-22 09:36:38 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "arist_id"
+    t.integer "genre_id"
+    t.integer "artist_id"
+  end
 
 end
